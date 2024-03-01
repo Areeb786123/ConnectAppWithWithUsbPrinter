1341c6838b62e5ee3982573999a84a8dc9b06338
areeb786 <areebrockzz786@gmail.com>
2024-03-01 18:26:31 +0530

    added  finaL CENTER

--- app/src/main/java/com/areeb/zebrastickerprinterdemo/MainActivity.kt
+++ app/src/main/java/com/areeb/zebrastickerprinterdemo/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.areeb.zebrastickerprinterdemo
 
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.hardware.usb.UsbConstants
 import android.hardware.usb.UsbDevice
 import android.hardware.usb.UsbDeviceConnection
 import android.hardware.usb.UsbEndpoint
+import android.hardware.usb.UsbInterface
 import android.hardware.usb.UsbManager
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
@@ -25,12 +33,14 @@ class MainActivity : AppCompatActivity() {
     companion object {
         private const val ZEBRA_PRINTER_VENDOR_ID = "2655"
         private const val ZEBRA_PRINTER_ID = "391"
+        private const val ACTION_USB_PERMISSION = "com.areeb.zebrastickerprinterdemo.USB_PERMISSION"
     }
 
     //
     private lateinit var usbManager: UsbManager
     private lateinit var usbDevice: UsbDevice
     private lateinit var endpoint: UsbEndpoint
+    private lateinit var usbInterface: UsbInterface
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -39,10 +49,28 @@ class MainActivity : AppCompatActivity() {
         getVendorId()
         findZebraPrinter()
         btn.setOnClickListener {
-            printToZebraPrinter()
+            if (usbDevice != null && usbManager.hasPermission(usbDevice)) {
+                startPrinting()
+            } else {
+                // Request permission if not granted
+                requestUsbPermission()
+            }
         }
     }
 
+
+    private fun requestUsbPermission() {
+        val permissionIntent = PendingIntent.getBroadcast(
+            this,
+            0,
+            Intent(ACTION_USB_PERMISSION),
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        val filter = IntentFilter(ACTION_USB_PERMISSION)
+        registerReceiver(usbPermissionReceiver, filter)
+
+        usbManager.requestPermission(usbDevice, permissionIntent)
+    }
 
     private fun getVendorId() {
         /**
@@ -88,19 +116,31 @@ class MainActivity : AppCompatActivity() {
                     "Areb",
                     "Interface $i - ${uintf.interfaceClass}, ${uintf.name}, ${uintf.endpointCount}"
                 )
-                for (j in 0 until uintf.endpointCount) {
-                    endpoint = uintf.getEndpoint(j)
-                    Log.e(
-                        "Areb",
-                        "   Endpoint $j - Address: ${endpoint.address}, Type: ${endpoint.type}"
-                    )
-                }
+
 
                 if (uintf.interfaceClass == UsbConstants.USB_CLASS_PRINTER) {
+                    usbInterface = uintf
                     Log.e("vishal", "Printer USB interface found")
-                    Toast.makeText(this, "connect to ${uintf.interfaceClass.toString()}", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        this,
+                        "connect to ${uintf.interfaceClass.toString()}",
+                        Toast.LENGTH_SHORT
+                    ).show()
 //                    this.usbInterface = uintf
 
+                    //find the endpooint
+                    for (j in 0 until uintf.endpointCount) {
+
+
+                        if(uintf.getEndpoint(j).type == UsbConstants.USB_ENDPOINT_XFER_BULK){
+                            endpoint = uintf.getEndpoint(j)
+                            break
+                        }
+                        Log.e(
+                            "Areb",
+                            "   Endpoint $j - Address: ${endpoint.address}, Type: ${endpoint.type}"
+                        )
+                    }
                     break
                 }
             }
@@ -109,6 +149,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // Call this function from where you want to start printing
+    private fun startPrinting() {
+        runBlocking {
+            launch(Dispatchers.IO) {
+                printToZebraPrinter()
+            }
+        }
+    }
 
     private fun printToZebraPrinter() {
         try {
@@ -120,11 +168,17 @@ class MainActivity : AppCompatActivity() {
                 outputStream.write("^XA^FO100,100^ADN,36,20^FDHello, Zebra!^FS^XZ".toByteArray())
                 outputStream.close()
 
+                connection.claimInterface(usbInterface, true)
+
+                val cut_paper = byteArrayOf(0x1D, 0x56, 0x41, 0x10)
+
                 connection.bulkTransfer(
                     endpoint, readZpl(zplFile),
                     zplFile.length().toInt(),
                     1000
                 )
+
+                connection.bulkTransfer(endpoint, cut_paper, cut_paper.size, 0)
 
                 connection.close()
             } else {
@@ -147,5 +201,49 @@ class MainActivity : AppCompatActivity() {
         fileInputStream.close()
         return bytes
     }
+
+    private val usbPermissionReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (intent?.action == UsbManager.ACTION_USB_DEVICE_ATTACHED) {
+                val device: UsbDevice? = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
+                if (device != null) {
+                    // Check if the attached device is your Zebra printer
+                    // Then handle permission request accordingly
+                }
+            } else if (intent?.action == ACTION_USB_PERMISSION) {
+                Log.e(
+                    "permosssAA",
+                    intent.toString()
+                )
+                if (true || intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                    // User has granted permission, proceed with further operations
+                    startPrinting()
+                } else {
+                    // User has denied permission, handle it accordingly
+                    Toast.makeText(
+                        this@MainActivity,
+                        "some error occur while permission",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            } else {
+                Toast.makeText(this@MainActivity, "receiver error", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val filter = IntentFilter()
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED)
+        filter.addAction(ACTION_USB_PERMISSION)
+        registerReceiver(usbPermissionReceiver, filter)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unregisterReceiver(usbPermissionReceiver)
+    }
+
 
 }
